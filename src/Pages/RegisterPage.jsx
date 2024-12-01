b0ddd5155c2737350c09314d646b3f7f05638e28
jjplaza-dev <jjplaza.dev@gmail.com>
2024-12-01 19:17:41 +0800

    minor updates

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function RegisterPage() {
+  const registerAccount = () => {
+    console.log(document.getElementById("usernameInput").value);
+  };
   return (
     <>
+      <label>Username: </label>
+      <input id="usernameInput" />
+      <label>Password: </label>
+      <input type="password" />
       <a href="/">already have an account?</a>
-      <button>Register</button>
+      <button onClick={registerAccount}>Register</button>
     </>
   );
 }
